e4a14b8da8f13719a6a36fc9d3fbae1913ce09d9
harlan.chen <harlan.chen@irdeto.com>
2017-05-18 15:04:25 +0800

    update 3_3.c

--- chapter_3/myexamples/3_3.c
+++ chapter_3/myexamples/3_3.c
@@ -1,10 +1,22 @@
 #include <fcntl.h>
 #include <stdio.h>
 int main(void)
-{   /**************************
-
-	  *************************/
-	int fd = open("test.txt",O_RDONLY);
+{   /*****************************************************************
+    /*int open(const char*path,int oflag,.../*mode_t mode*/);
+    /*int openat(int fd,const char*path,int oflag,.../*mode_t mode*/)
+	/****************************************************************/
+	/*FLAGS
+    /*  O_RDONLY read-only 
+    /*  O_WRONLY write-only
+    /*  O_RDWR  read and write
+    /*   most implementations define O_RDONLY as 0,O_WRONLY as 1,
+    /*   O_RDWR as 2.
+    /*  O_EXEC execute-only
+    /*  O_SEARCH  search-onlyad\
+    /****************************************************************/
+    
+    
+    int fd = open("test.txt",O_RDONLY);
 	printf("%d\n",fd);
 	int rv = write(fd,"test",4);//return -1 
 	printf("%d\n",rv);
