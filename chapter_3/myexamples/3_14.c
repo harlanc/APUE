7e9452c94789ed9994b8b5430f670fa95e5cd572
harlanc <wawacry@qq.com>
2017-05-25 11:09:00 +0800

    3-14.c

--- chapter_3/myexamples/3_14.c
+++ chapter_3/myexamples/3_14.c
@@ -1,19 +1,25 @@
 #include <fcntl.h>
 #include <stdio.h>
-
+#include <sys/stat.h>
 void printFD(int fd)
 {
     printf("The fd num is %d\n",fd);
 }
 
-void printFDI(int fd,int fdI)
-
+void printFDFlags(int fd,int fdflags)
 {
-    printf("The file description identifer of %d is %d\n",fd,fdI);
+    printf("The file description flags of  %d is %d\n",fd,fdflags);
 }
+
+void printFLFlags(int fd,int flflags)
+{
+    printf("The file status flags of  %d is %d\n",fd,flflags);
+}
+
 
 void F_DUPFD_fcntl()
 {
+    printf("F_DUPFD_fcntl()..........\n");
     int fd = open("/etc/passwd",O_RDONLY);
     int newfd = fcntl(fd,F_DUPFD,0);//equal to dup(fd)
     printFD(fd);
@@ -22,7 +28,7 @@ void F_DUPFD_fcntl()
     close(newfd);
 }
 
-void F_DUPFD_CLOEXEC_fcntl()
+/*void F_DUPFD_CLOEXEC_fcntl()
 {
     int fd = open("/etc/passwd",O_RDONLY);
     int newfd = fcntl(fd,F_DUPFD_CLOEXEC,0);
@@ -31,16 +37,42 @@ void F_DUPFD_CLOEXEC_fcntl()
     int newfdI = fcntl(newfd,F_GETFD,0);
     printFDI(newfd,newfdI);
 
+}*/
+void F_SET_FD_GET_FD_fcntl()
+{
+    printf("F_SETFD_GETFD_fcntl()..........\n");
+    int fd = open("/etc/passwd",O_RDONLY);
+    int fdflag = fcntl(fd,F_GETFD,0);
+    printFDFlags(fd,fdflag);
+    fcntl(fd,F_SETFD,1);
+    fdflag = fcntl(fd,F_GETFD,0);
+    printFDFlags(fd,fdflag);
 }
-
+/*********************************************************
+* note  this command can change only the O_APPEND,
+* O_ASYNC, O_DIRECT, O_NOATIME, and O_NONBLOCK flags
+*******************************************************/
+#define RWRWRW  (S_IRUSR|S_IWUSR| S_IRGRP|S_IWGRP|S_IROTH|S_IWOTH)
+void F_SETFL_GETFL_fcntl()
+{
+    printf("F_SETFL_GETFL_fcntl()..........\n");
+    int fd = open("/etc/passwd",O_RDONLY);
+    int flflag = fcntl(fd,F_GETFL);
+    printFLFlags(fd,flflag);
+    fcntl(fd,F_SETFL,O_APPEND);
+    flflag = fcntl(fd,F_GETFL);
+    printFLFlags(fd,flflag);
+}
 int main(void)
 {
     /**********************************************/
     /* int fcntl(int fd,int cmd,...*//* int arg*///)
     /**********************************************/
-    //F_DUPFD_fcntl();
+    F_DUPFD_fcntl();
 
-    F_DUPFD_CLOEXEC_fcntl();
+    //F_DUPFD_CLOEXEC_fcntl();
+    F_SET_FD_GET_FD_fcntl();
+    F_SETFL_GETFL_fcntl();
     return 0;
 
 
